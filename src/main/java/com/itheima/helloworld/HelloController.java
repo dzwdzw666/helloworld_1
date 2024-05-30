b484264c81e1256e37a097cc93b636f51076b03a
dzw <1759328812@qq.com>
2024-05-30 11:53:52 +0800

    第2次提交

--- src/main/java/com/itheima/helloworld/HelloController.java
+++ src/main/java/com/itheima/helloworld/HelloController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
-        System.out.println("Hello World1...");
+        System.out.println("Hello World2...");
         return "ITCAST OK";
     }
 }
